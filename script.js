89dd3de4b1d5f2812474b882ca4afc673cfd9681
Sára <sari.vogel.94@gmail.com>
2018-10-04 11:32:45 +0200

    Randomize background with 4 colors

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 
-console.log("It works");
+
+function changeColor () {
+    
+    var randomValue = Math.random();
+    console.log(randomValue);
+
+     if (randomValue <= 0.2) {
+        document.body.style.backgroundColor = "red";
+    }else if (randomValue <= 0.4) {
+        document.body.style.backgroundColor = "blue";
+    } else if (randomValue <= 0.6) {
+        document.body.style.backgroundColor = "yellow";
+    }
+    else{
+        document.body.style.backgroundColor = "green";
+    }
+
+}
+
+// Pass by value
+// string, number, boolean
+
+// Pass by reference
+// array, object
